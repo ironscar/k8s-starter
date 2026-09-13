9ffe60ad7ee98d54e7a3cc3c386c8cbc6f299a57
ironscar <uchakravarty29@gmail.com>
2026-09-13 21:29:16 +0530

    fix: cluster load scripts fixed for k3d

--- workloads/springweb/run/cluster-load.sh
+++ workloads/springweb/run/cluster-load.sh
@@ -1,8 +1,2 @@
-# needs to run in WSL env from repository root instead of app root
-cd ../..
-rm repository/springweb/k8s-dck-springweb-v0.0.1.tar
-docker save -o repository/springweb/k8s-dck-springweb-v0.0.1.tar k8s-dck-springweb:0.0.1
-cat repository/springweb/k8s-dck-springweb-v0.0.1.tar | nerdctl -n k8s.io load
-kubectl delete deployment springweb
-kubectl apply -f deployments/springweb/deployment.yaml
-cd workloads/springweb
+k3d image import k8s-dck-springweb:0.0.1 --cluster my-cluster
+kubectl rollout restart deployment/springweb
